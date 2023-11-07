fc695c87fd47e249b235e0007b9e2bab0a3680dc
Oduigwe Gerald <paulsongerald00@gmail.com>
2023-11-07 09:39:32 +0100

    worked on editing profile details

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import gerry from "./assets/gerry.jpg";
 import profileIcon from "./assets/profileIcon.svg";
 
 function Bio() {
+  const [form, setForm] = useState(false);
+  // const [name, setName] = useState("");
+  // const [about, setAbout] = useState("");
+  const [person, setPerson] = useState({
+    name: "Gerald",
+    about: "software Engineer @Google",
+  });
+
+  const setProfileForm = (e) => {
+    e.preventDefault();
+    setForm(true);
+  };
+  const setCloseForm = (e) => {
+    e.preventDefault();
+    setForm(false);
+  };
+
+  // USING THE VALUE PROPERTY ON THE INPUT FIELD
+  // const setProfile = (e) => {
+  //   e.preventDefault();
+  //   const newPerson = { name, about };
+  //   setPerson(newPerson);
+  //   setForm(false);
+  //   setName("");
+  //   setAbout("");
+  // };
+
+  //WITHOUT USING THE VALUE PROPERTY
+  const setProfile = (e) => {
+    e.preventDefault();
+    setPerson({
+      name: e.target.userName.value,
+      about: e.target.aboutUser.value,
+    });
+  };
+
   const EditForm = (
-    <form className="edit-bio-form" action="">
-      <input type="text" id="" placeholder="name" />
-      <input type="text" id="" placeholder="about you" /> <br />
-      <button className="cancel-button">cancel</button>
-      <button>Edit</button>
+    <form className="edit-bio-form" onSubmit={(e) => setProfile(e)}>
+      <input
+        type="text"
+        id=""
+        placeholder="name"
+        name="userName"
+        // value={name}
+        // onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        id=""
+        placeholder="about you"
+        name="aboutUser"
+        // value={about}
+        // onChange={(e) => setAbout(e.target.value)}
+      />{" "}
+      <br />
+      <button className="cancel-button" onClick={setCloseForm}>
+        cancel
+      </button>
+      <button type="submit">Save</button>
     </form>
   );
   return (
@@ -17,10 +70,10 @@ function Bio() {
         <img src={profileIcon} alt="profile" />
       </div>
       <div className="profile-info">
-        <p className="name">Gerald</p>
-        <p className="about">software developer @ gerry.io</p>
-        <button>Edit profile</button>
-        {EditForm}
+        <p className="name">{person.name}</p>
+        <p className="about">{person.about}</p>
+        {!form && <button onClick={setProfileForm}>Edit profile</button>}
+        {form && EditForm}
       </div>
     </section>
   );
--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import gerry from "./assets/gerry.jpg";
 import pexels2 from "./assets/pexels2.jpeg";
+import pexels3 from "./assets/pexels3.jpeg";
+import pexels4 from "./assets/pexels4.jpeg";
+
+const photos = [gerry, pexels2, pexels3, pexels4];
 
 function Gallary() {
   return (
@@ -8,10 +13,15 @@ function Gallary() {
       <label htmlFor="addPhoto">
         <i className="add-photo-button fas fa-plus-square" />
       </label>
-      <section className="gallary">
-        <div className="item">
-          <img src={pexels2} alt="mine" className="item-image" />
-        </div>
+      <section className="gallery">
+        {photos.map((photo) => {
+          return (
+            <div className="item">
+              <img src={photo} alt="mine" className="item-image" />
+              <button className="delete-button">delete</button>
+            </div>
+          );
+        })}
       </section>
     </>
   );
